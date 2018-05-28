d1e840ea06a895d5648866c0caba5841c61f434a
Носоленко Евгений Сергеевич <genyanosolenko@mail.ru>
2018-05-28 16:04:57 +0300

    Initial commit: window appears on the center, window minimized to system tray when user clicked "minimize" or "close" buttons, restoring window from system tray, shutdown application from system tray

---
+++ TimeTracker/Main.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace TimeTracker
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.systemTrayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.systemTrayIconContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.systemTrayMenuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.systemTrayMenuSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.systemTrayMenuClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.systemTrayIconContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // systemTrayIcon
+            // 
+            this.systemTrayIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.systemTrayIcon.BalloonTipText = "The application is working in the background";
+            this.systemTrayIcon.BalloonTipTitle = "Time Tracker";
+            this.systemTrayIcon.ContextMenuStrip = this.systemTrayIconContextMenu;
+            this.systemTrayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("systemTrayIcon.Icon")));
+            this.systemTrayIcon.Text = "Time Tracker";
+            this.systemTrayIcon.Visible = true;
+            this.systemTrayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.SystemTrayIcon_MouseDoubleClick);
+            // 
+            // systemTrayIconContextMenu
+            // 
+            this.systemTrayIconContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.systemTrayMenuOpen,
+            this.systemTrayMenuSeparator,
+            this.systemTrayMenuClose});
+            this.systemTrayIconContextMenu.Name = "systemTrayIconContextMenu";
+            this.systemTrayIconContextMenu.Size = new System.Drawing.Size(181, 76);
+            // 
+            // systemTrayMenuOpen
+            // 
+            this.systemTrayMenuOpen.Name = "systemTrayMenuOpen";
+            this.systemTrayMenuOpen.Size = new System.Drawing.Size(180, 22);
+            this.systemTrayMenuOpen.Text = "Open";
+            this.systemTrayMenuOpen.Click += new System.EventHandler(this.SystemTrayMenuOpen_Click);
+            // 
+            // systemTrayMenuSeparator
+            // 
+            this.systemTrayMenuSeparator.Name = "systemTrayMenuSeparator";
+            this.systemTrayMenuSeparator.Size = new System.Drawing.Size(177, 6);
+            // 
+            // systemTrayMenuClose
+            // 
+            this.systemTrayMenuClose.Name = "systemTrayMenuClose";
+            this.systemTrayMenuClose.Size = new System.Drawing.Size(180, 22);
+            this.systemTrayMenuClose.Text = "Exit";
+            this.systemTrayMenuClose.Click += new System.EventHandler(this.SystemTrayMenuClose_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Time Tracker";
+            this.systemTrayIconContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon systemTrayIcon;
+        private System.Windows.Forms.ContextMenuStrip systemTrayIconContextMenu;
+        private System.Windows.Forms.ToolStripMenuItem systemTrayMenuClose;
+        private System.Windows.Forms.ToolStripMenuItem systemTrayMenuOpen;
+        private System.Windows.Forms.ToolStripSeparator systemTrayMenuSeparator;
+    }
+}
+
---
+++ TimeTracker/Main.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeTracker
+{
+    public partial class Main : Form
+    {
+        #region Constructors
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Overridden Methods
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            // in case if user minimized window we need to put it in the system tray and hide it from taskbar
+            if (WindowState == FormWindowState.Minimized)
+                PutInTray();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // in case if user clicked "Close" button we need to minimize window and put it to the system tray
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private void SystemTrayIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // if application is in the system tray we need to restore it to normal size
+            if (WindowState == FormWindowState.Minimized)
+                OpenFromTray();
+        }
+
+        private void SystemTrayMenuClose_Click(object sender, EventArgs e)
+        {
+            // when user clicked "Exit" button from system tray we need to shutdown application
+            Application.Exit();
+        }
+
+        private void SystemTrayMenuOpen_Click(object sender, EventArgs e)
+        {
+            // just restore window to normal size if user clicked "Open" button from system tray context menu
+            OpenFromTray();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void PutInTray()
+        {
+            ShowInTaskbar = false;
+
+            systemTrayIcon.Visible = true;
+            systemTrayIcon.ShowBalloonTip(3000);
+        }
+
+        private void OpenFromTray()
+        {
+            WindowState = FormWindowState.Normal;
+            ShowInTaskbar = true;
+
+            systemTrayIcon.Visible = false;
+        }
+
+        #endregion
+    }
+}
